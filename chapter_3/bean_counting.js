fba0b41f0d8d3c69165f52b165395440e8efe995
Aaron McCollum <59707347+aaronmccollum@users.noreply.github.com>
2022-02-13 19:57:55 +0900

    Update bean_counting.js

--- chapter_3/bean_counting.js
+++ chapter_3/bean_counting.js
@@ -1,21 +1,21 @@
 const countBs = (str) => {
-  	let count = 0;
-	for (let i = 0; i <= str.length - 1; i++) {
+    let count = 0;
+    for (let i = 0; i <= str.length - 1; i++) {
     	if (str[i] === "B") {
-        	count += 1;
+            count += 1;
         };
     };
-  	return count;
+    return count;
 }
 
 const countChar = (str, char) => {
-	let count = 0;
-  	for (let i = 0; i <= str.length - 1; i++) {
+    let count = 0;
+    for (let i = 0; i <= str.length - 1; i++) {
     	if (str[i] === char) {
-        	count += 1; 
+            count += 1; 
         };
     };
-  	return count;
+    return count;
 }
 
 console.log(countBs("BBC"));
